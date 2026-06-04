ffcb3f1ad7b35a79392c261cb3e74592e07db7f7
devoreofox <232652342+devoreofox@users.noreply.github.com>
2026-06-04 17:40:42 -0300

    Added repo.json

--- LittleLuxuries/Plugin.cs
+++ LittleLuxuries/Plugin.cs
@@ -14,7 +14,7 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
     [PluginService] internal static IPluginLog Log { get; private set; } = null!;
 
-    private const string CommandName = "/pmycommand";
+    private const string CommandName = "/llux";
 
     public Configuration Configuration { get; init; }
 
